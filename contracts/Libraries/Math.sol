4bd28385bcdcb28eab302a7c71ab79829c399b8e
decanus <7621705+decanus@users.noreply.github.com>
2021-06-14 21:26:48 +0200

    added math lib

---
+++ contracts/Libraries/Math.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.4;
+
+contract Math {
+    function add(uint256 a, uint256 b) external pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) external pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) external pure returns (uint256) {
+        return a * b;
+    }
+
+    function sum(uint256[] calldata values)
+        external
+        pure
+        returns (uint256 ret)
+    {
+        for (uint256 i = 0; i < values.length; i++) {
+            ret += values[i];
+        }
+    }
+}
