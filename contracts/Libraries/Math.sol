ba0ec637d6384bcf6ff151e37a27b99d60812f1a
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2021-06-14 22:31:01 +0200

    feature/libraries (#1)
    * added math lib
    
    * using libs

--- contracts/Libraries/Math.sol
+++ contracts/Libraries/Math.sol
@@ -1,20 +1,16 @@
 pragma solidity ^0.8.4;
 
-contract SampleOps {
+contract Math {
     function add(uint256 a, uint256 b) external pure returns (uint256) {
         return a + b;
     }
 
-    function strlen(string calldata x) external pure returns (uint256) {
-        return bytes(x).length;
+    function sub(uint256 a, uint256 b) external pure returns (uint256) {
+        return a - b;
     }
 
-    function strcat(bytes calldata a, bytes calldata b)
-        external
-        pure
-        returns (bytes memory)
-    {
-        return abi.encodePacked(a, b);
+    function mul(uint256 a, uint256 b) external pure returns (uint256) {
+        return a * b;
     }
 
     function sum(uint256[] calldata values)
