49fb783678ba29f03187f21e91c20341737e4d19
Nick Johnson <arachnid@notdot.net>
2021-06-18 20:30:58 +1200

    Reuse state slots when possible. Fixes #18

--- contracts/CommandBuilder.sol
+++ contracts/CommandBuilder.sol
@@ -106,23 +106,30 @@ library CommandBuilder {
                     argptr := mload(add(output, 32))
                 }
                 require(argptr == 32, "Only one return value permitted");
-                bytes memory newstate = new bytes(output.length - 32);
-                memcpy(output, 32, newstate, 0, newstate.length);
-                state[idx & INDEX_MASK] = newstate;
+                bytes memory stateEntry = state[idx & INDEX_MASK];
+                // Only allocate new memory if we have to
+                if(stateEntry.length < output.length - 32) {
+                    stateEntry = state[idx & INDEX_MASK] = new bytes(output.length - 32);
+                }
+                assembly {
+                    // Set state entry length to output length
+                    mstore(stateEntry, sub(mload(output), 32))
+                }
+                // Copy the state data over
+                memcpy(output, 32, stateEntry, 0, stateEntry.length);
             }
         } else {
             // Single word
             require(output.length == 32, "Only one return value permitted");
 
-            if (state[idx & INDEX_MASK].length != 32) {
-                state[idx & INDEX_MASK] = new bytes(32);
+            bytes memory stateEntry = state[idx & INDEX_MASK];
+            if(stateEntry.length < 32) {
+                stateEntry = state[idx & INDEX_MASK] = new bytes(32);
             }
             assembly {
-                let stateptr := mload(
-                    add(add(state, 32), mul(and(idx, INDEX_MASK), 32))
-                )
                 let word := mload(add(output, 32))
-                mstore(add(stateptr, 32), word)
+                mstore(stateEntry, 32)
+                mstore(add(stateEntry, 32), word)
             }
         }
 
