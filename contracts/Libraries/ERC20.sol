67e7a91d2ff1c15fdc3d0d74e0c37892c66da6a7
mattdf <work@mattdf.com>
2021-06-19 16:32:48 +0200

    use IERC from zeppelin

--- contracts/Libraries/ERC20.sol
+++ contracts/Libraries/ERC20.sol
@@ -1,35 +1,6 @@
 pragma solidity ^0.8.4;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-}
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ERC20Ops {
     function totalSupply(address token) external view returns (uint256) {
