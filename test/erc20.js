80147a1a1c0bd4ae7fbb81dd02f2f6aab782fba6
Matthew Di Ferrante <mattdf@users.noreply.github.com>
2021-06-22 12:18:26 +0200

    feature/calltype-support (#34)
    * very draft calltype executor
    
    * fix memstore order
    
    * calltype tests, extended command support
    
    * rename calltype to flags

--- test/erc20.js
+++ test/erc20.js
@@ -29,10 +29,23 @@ describe("Executor", function () {
     executor = await Executor.deploy();
 
     /* transfer some balance to executor */
-    let ttx = await tokenContract.transfer(executor.address, amount);
+    let ttx = await tokenContract.transfer(executor.address, amount.mul(3));
     /* ensure that transfer was successful */
-    await expect(ttx).to.emit(tokenContract, "Transfer").withArgs(selfAddr, executor.address, amount);
+    await expect(ttx).to.emit(tokenContract, "Transfer").withArgs(selfAddr, executor.address, amount.mul(3));
   });
+
+  function execute(commands, state) {
+    let encodedCommands = commands.map(([target, func, inargs, outargs]) =>
+      ethers.utils.concat([
+        target.interface.getSighash(func),
+        inargs,
+        outargs,
+        target.address,
+      ])
+    );
+    return executor.execute(encodedCommands, state);
+  }
+  
 
   it("Should perform an ERC20 transfer", async () => {
     const planner = new weiroll.Planner();
@@ -44,6 +57,8 @@ describe("Executor", function () {
 
     const {commands, state} = planner.plan();
 
+    commands[0] = commands[0].slice(0, 10) + "00" + commands[0].slice(10, 22)  + commands[0].slice(24)
+
     const tx = await executor.execute(commands, state);
     await expect(tx)
       .to.emit(tokenContract, "Transfer")
@@ -51,5 +66,32 @@ describe("Executor", function () {
 
     const receipt = await tx.wait();
     console.log(`ERC20 transfer: ${receipt.gasUsed.toNumber()} gas`);
+  });
+
+  it("Should perform a DIRECT ERC20 transfer, without using lib", async () => {
+    let token = tokenContract.address;
+    let to = selfAddr;
+
+    let amt2 = ethers.utils.hexZeroPad(amount.toHexString(), 32);
+
+    const commands = [
+      [tokenContract, "transfer", "0x010001ffffffff", "0xff"]
+    ];
+    const state = [
+      // dest slot index
+      "0x000000000000000000000000" + to.slice(2) ,
+      // amt slot index
+      ethers.utils.hexZeroPad("0x01", 32),
+      // ret slot index
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+    ];
+
+    const tx = await execute(commands, state);
+    await expect(tx)
+      .to.emit(tokenContract, "Transfer")
+      .withArgs(executor.address, selfAddr, "0x1");
+
+    const receipt = await tx.wait();
+    console.log(`Direct ERC20 transfer: ${receipt.gasUsed.toNumber()} gas`);
   });
 });
