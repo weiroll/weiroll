57be94d0016a6fe1b2f4e04c2b82ed21dab43ca0
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2021-10-19 09:53:12 +0100

    added spdx (#54)

--- contracts/CommandBuilder.sol
+++ contracts/CommandBuilder.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 uint256 constant IDX_VARIABLE_LENGTH = 0x80;
--- contracts/Libraries/ERC20.sol
+++ contracts/Libraries/ERC20.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
