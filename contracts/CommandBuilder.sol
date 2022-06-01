bec31fdebf1eac6ed0c8e9e6aa768ec361e2c9c7
George Carder <georgercarder@gmail.com>
2022-06-01 09:27:21 -0700

    George/audit/issue/8 (#80)
    * del _unchecked fn call to save gas
    
    * put constants in library
    
    * put constants in library
    
    * call once outside branch
    
    * consolidate further

--- contracts/CommandBuilder.sol
+++ contracts/CommandBuilder.sol
@@ -2,12 +2,13 @@
 
 pragma solidity ^0.8.11;
 
-uint256 constant IDX_VARIABLE_LENGTH = 0x80;
-uint256 constant IDX_VALUE_MASK = 0x7f;
-uint256 constant IDX_END_OF_ARGS = 0xff;
-uint256 constant IDX_USE_STATE = 0xfe;
-
 library CommandBuilder {
+
+    uint256 constant IDX_VARIABLE_LENGTH = 0x80;
+    uint256 constant IDX_VALUE_MASK = 0x7f;
+    uint256 constant IDX_END_OF_ARGS = 0xff;
+    uint256 constant IDX_USE_STATE = 0xfe;
+
     function buildInputs(
         bytes[] memory state,
         bytes4 selector,
@@ -20,7 +21,7 @@ library CommandBuilder {
         uint256 idx;
 
         // Determine the length of the encoded data
-        for (uint256 i; i < 32; i=_uncheckedIncrement(i)) {
+        for (uint256 i; i < 32;) {
             idx = uint8(indices[i]);
             if (idx == IDX_END_OF_ARGS) break;
 
@@ -30,7 +31,6 @@ library CommandBuilder {
                         stateData = abi.encode(state);
                     }
                     count += stateData.length;
-                    unchecked{free += 32;}
                 } else {
                     // Add the size of the value, rounded up to the next word boundary, plus space for pointer and length
                     uint256 arglen = state[idx & IDX_VALUE_MASK].length;
@@ -39,7 +39,6 @@ library CommandBuilder {
                         "Dynamic state variables must be a multiple of 32 bytes"
                     );
                     count += arglen + 32;
-                    unchecked{free += 32;}
                 }
             } else {
                 require(
@@ -47,8 +46,9 @@ library CommandBuilder {
                     "Static state variables must be 32 bytes"
                 );
                 count += 32;
-                unchecked{free += 32;}
             }
+            unchecked{free += 32;}
+            unchecked{++i;}
         }
 
         // Encode it
@@ -57,7 +57,7 @@ library CommandBuilder {
             mstore(add(ret, 32), selector)
         }
         count = 0;
-        for (uint256 i; i < 32; i=_uncheckedIncrement(i)) {
+        for (uint256 i; i < 32;) {
             idx = uint8(indices[i]);
             if (idx == IDX_END_OF_ARGS) break;
 
@@ -68,7 +68,6 @@ library CommandBuilder {
                     }
                     memcpy(stateData, 32, ret, free + 4, stateData.length - 32);
                     free += stateData.length - 32;
-                    unchecked{count += 32;}
                 } else {
                     uint256 arglen = state[idx & IDX_VALUE_MASK].length;
 
@@ -84,7 +83,6 @@ library CommandBuilder {
                         arglen
                     );
                     free += arglen;
-                    unchecked{count += 32;}
                 }
             } else {
                 // Fixed length data; write it directly
@@ -92,8 +90,9 @@ library CommandBuilder {
                 assembly {
                     mstore(add(add(ret, 36), count), mload(add(statevar, 32)))
                 }
-                unchecked{count += 32;}
             }
+            unchecked{count += 32;}
+            unchecked{++i;}
         }
     }
 
@@ -177,10 +176,5 @@ library CommandBuilder {
                 )
             )
         }
-    }
-
-    function _uncheckedIncrement(uint256 i) private pure returns(uint256) {
-        unchecked {++i;}
-        return i;
     }
 }
